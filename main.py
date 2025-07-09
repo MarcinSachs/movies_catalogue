6063d08a100ed04e7521530df4e5f0a4cb35dfb6
Marcin Sachs <marcinsachs@gmail.com>
2025-07-09 20:36:35 +0200

    Create templates and base functionality

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
     movies = []
-    return render_template("homepage.html", movies=range(10))
+    return render_template("homepage.html", movies=range(8))
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
