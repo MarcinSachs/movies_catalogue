32ef3fe965693238078393c8e31016d1d475ac3e
Marcin Sachs <marcinsachs@gmail.com>
2025-07-20 19:02:51 +0200

    Translation completed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import tmdb_client as tmdb_client
-from flask import request
+from flask import request, redirect, url_for, make_response, g
 from flask_babel import Babel, _
 
 
@@ -16,15 +16,23 @@ app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'locales'
 
 
 def get_locale():
-    # return request.accept_languages.best_match(app.config['LANGUAGES'].keys())
-    return 'pl_PL'
+    if 'lang' in request.cookies:
+        return request.cookies['lang']
+    return request.cookies['lang']
 
 
 babel = Babel(app, locale_selector=get_locale)
 
 
+@app.before_request
+def before_request():
+    g.lang_code = get_locale()
+    g.api_language = lang_code_to_api_language(g.lang_code)
+
+
 @app.route('/')
 def homepage():
+    g.lang_code = get_locale()
     selected_list_type = request.args.get('list_type', 'popular')
     available_lists = {
         "popular": _("Popular"),
@@ -34,10 +42,21 @@ def homepage():
     }
     if selected_list_type not in available_lists:
         selected_list_type = "popular"
-
-    movies = tmdb_client.get_movies(8, selected_list_type)
+    movies = tmdb_client.get_movies(
+        8, selected_list_type, lang_code=g.api_language)
     return render_template("homepage.html", movies=movies, available_lists=available_lists,
                            list_type=selected_list_type)
+
+
+@app.route('/set_language', methods=['POST'])
+def set_language():
+    lang_code = request.form['lang']
+    if lang_code in app.config['LANGUAGES']:
+        response = make_response(redirect(url_for('homepage')))
+        response.set_cookie('lang', lang_code)
+        return response
+    # Redirect back if the language is invalid
+    return redirect(url_for('homepage'))
 
 
 @app.route('/contact')
@@ -52,8 +71,8 @@ def about():
 
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
-    movie = tmdb_client.get_single_movie(movie_id)
-    movie_info = tmdb_client.get_movie_info(movie)
+    movie = tmdb_client.get_single_movie(movie_id, lang_code=g.api_language)
+    movie_info = tmdb_client.get_movie_info(movie, lang_code=g.api_language)
     return render_template("movie_details.html", movie_info=movie_info)
 
 
@@ -62,7 +81,8 @@ def search():
     search_query = request.args.get("query", "")
     movies = []
     if search_query:
-        movies = tmdb_client.search_movies(search_query)
+        movies = tmdb_client.search_movies(
+            search_query, lang_code=g.api_language)
     return render_template("search.html", movies=movies, search_query=search_query)
 
 
@@ -71,6 +91,10 @@ def utility_processor():
     def tmdb_image_url(path, size):
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
+
+
+def lang_code_to_api_language(lang_code):
+    return lang_code.replace('_', '-')
 
 
 if __name__ == '__main__':
--- tmdb_client.py
+++ tmdb_client.py
@@ -30,9 +30,12 @@ def get_popular_movies():
         print(f"API request failed: {e}")
         # Return a default structure so the app doesn't crash.
         return {"results": []}
-    
-def get_movies_list(list_type):
-    endpoint = f"{API_BASE_URL}/movie/{list_type}?language=pl-PL"
+
+
+def get_movies_list(list_type, lang_code):
+    print(
+        f"Fetching movies list of type: {list_type} with language code: {lang_code}")
+    endpoint = f"{API_BASE_URL}/movie/{list_type}?language={lang_code}"
     api_token = os.environ.get("TMDB_API_TOKEN")
     if not api_token:
         # If the token is not found raise an error.
@@ -54,8 +57,8 @@ def get_movies_list(list_type):
         return {"results": []}
 
 
-def get_single_movie(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}?language=pl-PL"
+def get_single_movie(movie_id, lang_code=None):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}?language={lang_code}"
     api_token = os.environ.get("TMDB_API_TOKEN")
     headers = {
         "Authorization": f"Bearer {api_token}"
@@ -84,8 +87,8 @@ def get_movie_watch_providers(movie_id):
     return response.json().get("results", {}).get("PL", {}).get("flatrate", [])
 
 
-def get_single_movie_cast(movie_id):
-    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?language=pl-PL"
+def get_single_movie_cast(movie_id, lang_code):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits?language={lang_code}"
     api_token = os.environ.get("TMDB_API_TOKEN")
     headers = {
         "Authorization": f"Bearer {api_token}"
@@ -94,8 +97,9 @@ def get_single_movie_cast(movie_id):
     return response.json().get("cast", [])
 
 
-def get_movies(how_many, list_tape="popular"):
-    data = get_movies_list(list_tape)
+def get_movies(how_many, list_tape="popular", lang_code=None):
+    print(lang_code)
+    data = get_movies_list(list_tape, lang_code)
     movies = data.get("results", [])
     random.shuffle(movies)
     return movies[:how_many]
@@ -105,20 +109,20 @@ def get_poster_url(poster_path, poster_size="w342"):
     return f"{IMAGE_BASE_URL}{poster_size}{poster_path}"
 
 
-def get_movie_info(movie):
+def get_movie_info(movie, lang_code=None):
     id = movie.get("id")
     title = movie.get("title")
-    tigline = movie.get("tagline")
+    tagline = movie.get("tagline")
     description = movie.get("overview")
     images = get_movie_images(id)
-    cast = get_single_movie_cast(id)
+    cast = get_single_movie_cast(id, lang_code=lang_code)
     budget = movie.get("budget")
     genres = movie.get("genres")
     providers = get_movie_watch_providers(id)
     return {
         "id": id,
         "title": title,
-        "tagline": tigline,
+        "tagline": tagline,
         "description": description,
         "images": images,
         "cast": cast,
@@ -126,8 +130,10 @@ def get_movie_info(movie):
         "genres": genres,
         "providers": providers
     }
-def search_movies(search_query):
-    endpoint = f"https://api.themoviedb.org/3/search/movie?query={search_query}&language=pl-PL"
+
+
+def search_movies(search_query, lang_code=None):
+    endpoint = f"https://api.themoviedb.org/3/search/movie?query={search_query}&language={lang_code}"
     api_token = os.environ.get("TMDB_API_TOKEN")
     headers = {
         "Authorization": f"Bearer {api_token}"
